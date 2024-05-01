f1e070bd23abd6a9ac9fc409ce73e78e13bf3fe8
Jack Xul <xyz1357997531@gmail.com>
2024-05-01 14:00:46 +0800

    add code

---
+++ code/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+//define
+#define Lenth 500
+#define Speed 2 /* (m/s) */
+#define Droplet 330 /* (m^3/s) */
+#define Height 5 /* (m) */
+#define Spacing 1 /* (m) */
+
+//main
+int main(argc, argv[])
+{
+	int Charge_time = 0;
+	int Current_Height = 0;
+	int Current_Speed = 0;
+
+	
+
+
+
+	
+	return 0;
+}
+
+
