5f474978bf4e6d3fa048fe765046a9ff95cb28a9
Jack Xul <xyz1357997531@gmail.com>
2024-05-01 16:20:28 +0800

    Add charging boader

--- code/src/main.c
+++ code/src/main.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 //define
-#define Grid_Length 500
-#define Grid_Height 500
+#define Grid_Length 50
+#define Grid_Height 50
 #define Speed 2 /* (m/s) */
 #define Droplet 330 /* (m^3/s) */
 #define Height 5 /* (m) */
@@ -17,35 +17,36 @@ int main(int argc,char *argv[])
 	int Current_Height = 0;
 	int Current_Speed = 0;
 	//malloc 2D array
-	int **Array = (int **)malloc(Grid_Length * sizeof(int *));
+	int **Array = (int **)malloc((Grid_Length + 2) * sizeof(int *));
 	for (int i = 0; i < Grid_Height; i++)
 	{
-		Array[i] = (int *)malloc(Grid_Height * sizeof(int));
+		Array[i] = (int *)malloc((Grid_Height + 2) * sizeof(int));
 	}
 	//initialize array	
 	for (int i = 0; i < Grid_Length; i++)
 	{
-		if(i <= 2){
 
 			for(int j = 0; j < Grid_Height; j++)
 			{
-				Array[i][j] = 2;
+				//boarder
+				if(i == 0 || i == Grid_Length - 1 || j == 0 || j == Grid_Height - 1){
+					Array[i][j] = -1;
+				//double spray zone
+				}else if(i > 0 && i <= 2 || i < Grid_Length - 1 && i >= Grid_Length - 3 || j > 0 && j <= 2 || j < Grid_Height - 1 && j >= Grid_Height - 3){
+					Array[i][j] = 2;
+				//single spray zone
+				}else{
+					Array[i][j] = 1;
+				}
 			}
-		}else{
-			for(int j = 0; j < Grid_Height; j++)
-			{
-				Array[i][j] = 1;
-			}
-		}
 	}
-
 
 	//print array
 	for (int i = 0; i < Grid_Length; i++)
 	{
 		for (int j = 0; j < Grid_Height; j++)
 		{
-			printf("%d ", Array[i][j]);
+			printf("%2d ", Array[i][j]);
 		}
 		printf("\n");
 	}
