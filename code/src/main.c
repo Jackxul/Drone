e8296bc4590c00a876227cbcc646cef892558385
Jack Xul <xyz1357997531@gmail.com>
2024-07-01 14:32:55 +0800

    add GRIZD_SIZE in makefile

--- code/src/main.c
+++ code/src/main.c
@@ -5,8 +5,17 @@
 #include<time.h>
 
 //define
+#ifdef TEST_MODE
 #define Grid_Length 10
 #define Grid_Height 10
+#else
+#define Grid_Length 30
+#define Grid_Height 30
+#endif
+#ifdef GRID_SIZE
+#define Grid_Length GRID_SIZE
+#define Grid_Height GRID_SIZE
+#endif
 #define Speed 2 /* (m/s) */		// = (1m/0.5s)
 #define Droplet 330 /* (m^3/s) */
 #define Height 5 /* (m) */
@@ -84,48 +93,64 @@ float zigzag(int **arr, int arr_length, int arr_height){	//zigzag
 			if(arr[i][j] >= 1 && life > 0){
 				arr[i][j] = 0;
 				printf("Current position: %d, %d --> %d\n", i, j, arr[i][j]);
+#ifdef DEBUG_MODE
 				printf("i --> %d\n", i);
 				printf("j --> %d\n", j);
+#endif
 				trip++;
 				life -= 0.5;
-				#ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
 				print_array(arr, arr_length, arr_height);
-				#endif
+#elif TEST_MODE
+				print_array(arr, arr_length, arr_height);
+#endif
 			}else if(arr[i][j] >= 1 && life <= 0){
 				printf("Battery is out of power\n");
 				//save the current position into the queue
 				power_out_queue[0][0] = i; //Grid_Height
 				power_out_queue[0][1] = j; //Grid_Length
-
+				/*
+				 *
+				 *
+				 *do charging and calculation
+				 *
+				 *
+				 *
+				 *
+				 * */
 				exit(0);
 			}else if(dir_y & (j == arr_length - 1)){
+#ifdef DEBUG_MODE
 				printf("temp change = %d\n", temp);
+#endif
 				temp = arr_length - 2;
 				dir_x = -1;
 				dir_y = false;
-				printf("check point --> \n");
-				//print dir_y
+				//printf("check point --> \n");
+#ifdef DEBUG_MODE
 				printf("dir_y = %d\n", dir_y);
 				printf("j = %d\n", j);
-				continue;
+#endif
 			}else if(arr[i][j] < 1){
+#ifdef DEBUG_MODE
 				printf("i = %d\n",i);
 				printf("j = %d\n",j);
 				printf("arr[i][j] = %d\n", arr[i][j]);
 				printf("temp = %d\n", temp);
 				printf("Worng position\n");
+#endif
 				break;
 
 			}
 			if(!(dir_y) & (j == 1)){
 				temp = 1;
-				printf("temp change = %d\n", temp);
 				dir_x = 1;
 				dir_y = true;
-				//print dir_y
+#ifdef DEBUG_MODE
+				printf("temp change = %d\n", temp);
 				printf("dir_y = %d\n", dir_y);
 				printf("j = %d\n", j);
-				continue;
+#endif
 			}
 			//printf("check point 2\n");
 			//int static count = 0;
@@ -145,45 +170,69 @@ float zamboni(int **arr, int arr_length, int arr_height){	//zamboni
 	int dir_x = 1;//direction variable (use to control the direction of zigzag)
 	bool dir_y = true;//direction variable (use to check the direction of zigzag)
 	for(int i = 1; i < arr_height; i++){
-		for(int j = temp; j < arr_length && j > 0; j + dir_x){
+		for(int j = temp; j < arr_length && j > 0; j += dir_x){
+			//debug code
+			//debug code
 			if(arr[i][j] >= 1 && life > 0){
-				arr[i][j]--;
+				arr[i][j] = 0;
 				printf("Current position: %d, %d --> %d\n", i, j, arr[i][j]);
 				printf("i --> %d\n", i);
 				printf("j --> %d\n", j);
 				trip++;
 				life -= 0.5;
+#ifdef DEBUG_MODE
+				print_array(arr, arr_length, arr_height);
+#elif TEST_MODE
+				print_array(arr, arr_length, arr_height);
+#endif
 			}else if(arr[i][j] >= 1 && life <= 0){
 				printf("Battery is out of power\n");
 				//save the current position into the queue
 				power_out_queue[0][0] = i; //Grid_Height
 				power_out_queue[0][1] = j; //Grid_Length
-				break;
-			}else if(arr[i][j] >= 1 && (dir_y & (j == arr_length - 1))){
-				temp = arr_length - 1;
+				/*
+				 *
+				 *
+				 *do charging and calculation
+				 *
+				 *
+				 *
+				 *
+				 * */
+				exit(0);
+			}else if(dir_y & (j == arr_length - 1)){
+				printf("temp change = %d\n", temp);
+				temp = arr_length - 2;
 				dir_x = -1;
 				dir_y = false;
-				break;
-			}else if(arr[i][j] >= 1 && (!(dir_y) & (j == 1))){
-				temp = 1;
-				dir_x = 1;
-				dir_y = true;
-				break;
+				printf("check point --> \n");
+				//print dir_y
+				printf("dir_y = %d\n", dir_y);
+				printf("j = %d\n", j);
 			}else if(arr[i][j] < 1){
 				printf("i = %d\n",i);
 				printf("j = %d\n",j);
 				printf("arr[i][j] = %d\n", arr[i][j]);
 				printf("temp = %d\n", temp);
 				printf("Worng position\n");
-				//break;
+				break;
 
+			}
+			if(!(dir_y) & (j == 1)){
+				temp = 1;
+				printf("temp change = %d\n", temp);
+				dir_x = 1;
+				dir_y = true;
+				//print dir_y
+				printf("dir_y = %d\n", dir_y);
+				printf("j = %d\n", j);
 			}
 			//printf("check point 2\n");
 			//int static count = 0;
 			//int staprintf("count: %d\n", ++count);
 		}
-
 	}
+	printf("life: %f\n", life);
 	return 0;
 }
 //main
